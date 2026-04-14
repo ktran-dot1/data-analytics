b7e4dd57395e753df852731c55de0580d5363710
ktran-dot1 <kevintr04@gmail.com>
2026-04-13 18:29:06 -0700

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,8 @@
 /*
 This sakila database is used to collect information about actors and the films they appear in.
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved [number] records
+
+SELECT * FROM film; -- Retrieved [number] records
