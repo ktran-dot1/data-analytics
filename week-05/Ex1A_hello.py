9b28c0e584bb2b55ee79464233ce9b1c87dde276
ktran-dot1 <kevintr04@gmail.com>
2026-05-04 11:52:54 -0700

    practice with variables

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,18 @@
-print('Hello world!')
+print('Hello world!') 
+
+# Practicing with variables
+
+message = 'Hello world!'
+print(message)
+    # 'Hellow world' prints twice because it is running both code.
+
+dollars = 3
+cents = .50
+print(dollars + cents)
+    # the results are 3.5. it added the value in dollars and cents together.
+cents = cents + .25
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + " " + c_str)
