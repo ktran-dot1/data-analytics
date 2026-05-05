66d4898fdddb41bfc3f4260aeb20652d77dd1716
ktran-dot1 <kxtran01@gmail.com>
2026-05-05 15:14:52 -0700

    Add variable definitions and explanations for customer information

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,30 @@
+# Create variable names for Customer Infomation
+
+id = '' # Customer ID
+name = ' ' # Customer Name
+gender = ' ' # Customer gender
+birthday = ' '  # Customer date of birth
+dl_number = ' '  # Driver's license number
+policy_number = ' ' # Auto policy number
+
+# 3. An assumption I made about the type of value the variable might need to contain, is
+# if the value will be changed. For example, Driver's license number will not change, so I think
+# making it into a string will be better
+
+# 4. Create variables for myself
+name = 'Kevin Tran'
+city = 'Union City'
+state = "California"
+
+# Lab 2
+# a) What is the full list of reserved words that can't be used for variable names?
+    # 33 words. False, None, True, and, as, assert, break, class, continue, def, del, elif, else
+    # except, finally, for, form, global, if, import, in, is, lambda, nonlocal, not, or, pass
+    # raise, return, try, while, with, and yield
+
+# b) Pick 5 of the reserved words and explain how it is used as a keyword in python
+    # def: The def function is used to define a function in python
+    # if: An if statement is used to make a conditional statement
+    # raise: The raise statement is used to raise an error
+    # none: The none is an object that represents the absence of a value
+    # del: The del statement is used to delete an object in python
