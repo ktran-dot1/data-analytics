86f53fc06d8bf318820f289f4e3cfbb854661a61
ktran-dot1 <kxtran01@gmail.com>
2026-05-07 11:59:58 -0700

    Found the area of a rectangle

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,14 @@
+# Calculate the area of a rectangle: Length * width.
+# Rectangle dimensions corresponds to your birthday.
+    # Month: 1, Day: 1
+
+# Create variables for the sides
+side_a = 1  # Will not be a rectangle, It will be a square
+side_b = 1
+
+# Calculate area
+area_of_rectangle = side_a * side_b
+
+print(f'Side A is {side_a}')
+print(f'Side B is {side_b}')
+print(f'The area of the square is {area_of_rectangle}')
