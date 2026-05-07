279b2464f6a44317946e9bda969c5e09779490bd
ktran-dot1 <kxtran01@gmail.com>
2026-05-07 11:44:21 -0700

    calculated my networth given my assets and debts

---
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,33 @@
+# What are assets that need to be included in the net worth.
+    # Cash, Savings accounts, stocks, Vehicles, real estate, retirement accounts, etc
+# What are debts?
+    # Mortgage, Loan, credit card balance, etc
+# Net worth = Assets - Debts.
+
+# Create Variables for assets
+
+cash = 5000
+checking_account = 1000
+savings_account = 20000
+stocks = 5000
+car = 30000
+retirement_fund = 5000
+
+# Create Variables for debts
+mortgage = 15000
+credit_balance = 1000
+loan = 6500
+student_loan = 15000
+
+# Calculate total assets
+total_assets = cash + checking_account + savings_account + stocks + car + retirement_fund
+
+# Calculate total debts
+total_debts = mortgage + credit_balance + loan + student_loan
+
+# Calculate net worth
+net_worth = total_assets - total_debts
+
+print(f'Your total assets are {total_assets}')
+print(f'Your total debts are {total_debts}')
+print(f'Your net worth is {net_worth}')
