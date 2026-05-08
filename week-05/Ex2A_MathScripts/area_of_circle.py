f60d67f7223671ea4188b0fbaf4a4acfee0dd1f2
ktran-dot1 <kxtran01@gmail.com>
2026-05-08 12:06:42 -0700

    area of a circle

---
+++ week-05/Ex2A_MathScripts/area_of_circle.py
@@ -0,0 +1,7 @@
+# finding the area of a circle
+import math
+
+diameter = 1
+radius = diameter / 2
+area = math.pi * radius ** 2
+print('The area of a circle with radius', radius, 'is', f'{area:.2f}')
