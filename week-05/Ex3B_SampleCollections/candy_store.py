c140be75526d3b031e02a01670106eacc7b062e3
ktran-dot1 <kxtran01@gmail.com>
2026-05-11 00:52:01 -0700

    completed candy store

---
+++ week-05/Ex3B_SampleCollections/candy_store.py
@@ -0,0 +1,12 @@
+candy = ("Jelly Beans", "Sour Strips", "Hi-Chews")
+
+flavor = ("Strawberry", "Mango", "Peach")
+
+candies_InStock = {
+    candy[0] + " " + flavor[0],
+    candy[1] + " " + flavor[1],
+    candy[2] + " " + flavor[2]
+}
+
+print("Today's candy options include: ")
+print(candies_InStock)
